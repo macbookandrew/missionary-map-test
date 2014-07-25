27fcb9b54ea531c3a8262b16189e144ed75a2885
Andrew Minion <andrew@andrewrminion.com>
2014-07-24 21:26:52 -0400

    cluster markers - use minified markercluster rather than src

--- js/map-project.js
+++ js/map-project.js
@@ -5,7 +5,7 @@ L.Icon.Default.imagePath = 'images';
 
 // @codekit-prepend "leaflet.js";
 // @codekit-prepend "missionary-list.js";
-// @codekit-prepend "../bower_components/leaflet.markercluster/dist/leaflet.markercluster-src.js";
+// @codekit-prepend "../bower_components/leaflet.markercluster/dist/leaflet.markercluster.js";
 
 // create a map in the "map" div, set the view to a given place and zoom
 var map = L.map('map').setView([0,0], 1);
