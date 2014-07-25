18c11611f0ccaee45ab1d4f9075686773ce46fab
Andrew Minion <andrew@andrewrminion.com>
2014-07-24 20:51:55 -0400

    modularize - move scripts out of index.html
    move scripts to their own files out of index.html

--- js/map-project.js
+++ js/map-project.js
@@ -4,3 +4,12 @@
 L.Icon.Default.imagePath = 'images';
 
 // @codekit-prepend "leaflet.js";
+// @codekit-append "missionary-list.js";
+
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map').setView([0,0], 1);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
