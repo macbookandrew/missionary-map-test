3078517b29d939761ee6d5636d82f3f3ff83a77c
Andrew Minion <andrew@andrewrminion.com>
2014-07-26 14:24:53 -0400

    sidebar - show content from the marker
    extend the marker class to include more info about each missionary
    on click, set the sidebar content and show it

--- js/map-project.js
+++ js/map-project.js
@@ -3,7 +3,7 @@
 // set imagePath default
 L.Icon.Default.imagePath = 'images';
 
-// @codekit-prepend "leaflet.js";
+// @codekit-prepend "leaflet-src.js";
 // @codekit-prepend "missionary-list.js";
 // @codekit-prepend "../bower_components/leaflet.markercluster/dist/leaflet.markercluster.js";
 // @codekit-prepend "L.Control.Sidebar.js";
@@ -20,20 +20,38 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 var sidebar = L.control.sidebar('sidebar', {
     position: 'left'
 });
-
 map.addControl(sidebar);
 
 // cluster nearby markers and display markers
 var markers = new L.MarkerClusterGroup();
 
+// extend marker class with more information
+customMarker = L.Marker.extend({
+    options: {
+        missionaryImgSrc: 'URL of image',
+        missionaryAddress: 'city/country'
+    }
+});
+
+// loop through missionaries and create markers
 for (var i = 0; i < missionaryPoints.length; i++) {
     var a = missionaryPoints[i];
+    var latitude = a[0];
+    var longitude = a[1];
     var title = a[2];
-    var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title }).on('click', function () {
-            sidebar.toggle();
-        });;
-    marker.bindPopup(title);
+    var missionaryImgSrc = a[3];
+    var missionaryAddress = a[4];
+    var marker = new customMarker([latitude, longitude], {
+        title: title,
+        missionaryImgSrc: missionaryImgSrc,
+        missionaryAddress: missionaryAddress
+    }).on('click', function () {
+        var sidebarContent = '<h1>' + this.options.title + '</h1><img src="' + this.options.missionaryImgSrc + '" /><address>' + this.options.missionaryAddress + '</address>';
+        sidebar.setContent( sidebarContent );
+        sidebar.show();
+    });
     markers.addLayer(marker);
 }
 
+// add markers to map
 map.addLayer(markers);
