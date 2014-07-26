8adaa18b29392dceec79e868c140cdc41ca9d358
Andrew Minion <andrew@andrewrminion.com>
2014-07-26 14:24:03 -0400

    missionary list - strip HTML out
    remove HTML and store just the values needed

--- js/missionary-list.js
+++ js/missionary-list.js
@@ -1,8 +1,8 @@
 var missionaryPoints = [
 
-[35.24272, -81.599412, '<img class="popup-photo" src="http://wwntbm.com/wp-content/uploads/2012/01/Eiben-Gabriel-300x268.jpg" /><h2>Gabriel and Susan Eiben</h2><address>Shelby, NC</address>'], //Eibens
-[35.959541, -86.53159, '<img class="popup-photo" src="http://wwntbm.com/wp-content/uploads/2011/12/Whetstone-Danny-300x252.jpg" /><h2>Danny and Darlene Whetstone</h2><address>Smyrna, TN</address>'], // Whetstones
-[39.671572, -76.056651, '<img class="popup-photo" src="http://wwntbm.com/wp-content/uploads/2012/01/Bibey-Richard1-300x236.jpg" /><h2>Richard and Andrea Bibey</h2><address>Rising Sun, MD</address>'], //Bibeys
-[6.98, 158.19, '<img class="popup-photo" src="http://wwntbm.com/wp-content/uploads/2012/01/Boylston-John-300x200.jpg" /><h2>John and Christa Boylston</h2><address>Kolonia, Pohnpei, FM</address>'], // Boylstons
-[9.78, 147.76, '<img class="popup-photo" src="http://wwntbm.com/wp-content/uploads/2013/10/Wagar-Summer-2013.png" /><h2>Josh and Sara Wagar</h2><address>Chuuk, Micronesia</address>'], // Wagars
+[35.24272, -81.599412, 'Gabriel and Susan Eiben', 'http://wwntbm.com/wp-content/uploads/2012/01/Eiben-Gabriel-300x268.jpg', 'Shelby, NC'], //Eibens
+[35.959541, -86.53159, 'Danny and Darlene Whetstone', 'http://wwntbm.com/wp-content/uploads/2011/12/Whetstone-Danny-300x252.jpg', 'Smyrna, TN'], // Whetstones
+[39.671572, -76.056651, 'Richand and Andrea Bibey', 'http://wwntbm.com/wp-content/uploads/2012/01/Bibey-Richard1-300x236.jpg', 'Rising Sun, MD'], //Bibeys
+[6.98, 158.19, 'John and Christa Boylston', 'http://wwntbm.com/wp-content/uploads/2012/01/Boylston-John-300x200.jpg', 'Kolonia, Pohnpei, FM'], // Boylstons
+[9.78, 147.76, 'Josh and Sara Wagar', 'http://wwntbm.com/wp-content/uploads/2013/10/Wagar-Summer-2013.png', 'Chuuk, Micronesia'], // Wagars
 ];
